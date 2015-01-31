35e1851be29e243cb114a1ef6726e663ba81ab0a
chenupt <chenupt@gmail.com>
2015-01-31 21:06:29 +0800

    add viewpager

---
+++ sample/src/main/java/github/chenupt/springindicator/sample/MainActivity.java
@@ -0,0 +1,56 @@
+package github.chenupt.springindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+import github.chenupt.multiplemodel.viewpager.ModelPagerAdapter;
+import github.chenupt.multiplemodel.viewpager.PagerModelManager;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        PagerModelManager manager = new PagerModelManager();
+        manager.addCommonFragment(GuideFragment.class, getTitles(), getTitles());
+        ModelPagerAdapter adapter = new ModelPagerAdapter(getSupportFragmentManager(), manager);
+        viewPager.setAdapter(adapter);
+    }
+
+    private List<String> getTitles(){
+        return Lists.newArrayList("ListView", "RecyclerView", "GridView", "ScrollView", "WebView");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
